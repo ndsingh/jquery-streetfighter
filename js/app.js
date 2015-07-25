eb8fb42b164d6c287732c01d90a0275bd884b2ef
ndsingh <ndsingh987@gmail.com>
2015-07-25 12:00:51 -0600

    Keydown event

--- js/app.js
+++ js/app.js
@@ -3,6 +3,22 @@ $(document).ready(function(){
 		$('.ryu-still').hide();
 		$('.ryu-ready').show();
 	});
+	$(document).keydown(function(e){
+		if(e.which == 88) {
+			$('.ryu-ready').hide();
+			$('.ryu-still').hide();
+			$('.ryu-throwing').hide();
+			$('.ryu-cool').show();
+		}
+	});
+	$(document).keyup(function(e){
+		if(e.which == 88) {
+			$('.ryu-ready').show();
+			$('.ryu-still').hide();
+			$('.ryu-throwing').hide();
+			$('.ryu-cool').hide();
+		}
+	});
 	$('.ryu').mouseleave(function(){
 		$('.ryu-ready').hide();
 		$('.ryu-still').show();
@@ -25,6 +41,7 @@ $(document).ready(function(){
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
 	});
+	
 });
 function playHadouken () {
 	$('#hadouken-sound')[0].volume=0.5;
